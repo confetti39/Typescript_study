52e0f5a797e010706cff60b53bbbc842b926445b
“깃 <hy04730@naver.com>
2022-07-08 16:55:10 +0900

    add: basic grammar 05(overloading)

--- nomadcoders/basic/5.ts
+++ nomadcoders/basic/5.ts
@@ -3,9 +3,32 @@
 // }
 
 //call signitures
-type Add = (a: number, b: number) => number;
-// type Add = { (a: number, b: number): number };
-const add: Add = (a, b) => a + b;
+// type Add = (a: number, b: number) => number;
 // Call(=function) Signiture란 함수의 매개변수와 반환 값의 타입을 모두 type으로 미리 선언하는 것이다.
 // React에서 함수로 props를 보낼 때, 어떻게 작동할지 미리 설계 가능함.
 // 먼저 함수의 타입을 설정하고 코드를 구현할 수 있어서 장점임.
+
+type Add = {
+  (a: number, b: number): number;
+  (a: number, b: number, c: number): number;
+};
+const add: Add = (a, b, c?: number) => {
+  if (c) return a + b + c;
+  return a + b;
+};
+
+type Config = {
+  path: string;
+  state: object;
+};
+type Push = {
+  (path: string): void;
+  (config: Config): void;
+};
+
+const push: Push = (config) => {
+  if (typeof config === "string") console.log(config);
+  else {
+    console.log(config.path);
+  }
+};
